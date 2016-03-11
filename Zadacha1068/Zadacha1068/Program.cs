041b27954552ec5dc356d78bf2603b3b5de9b54f
MKruglov <krumih@mail.ru>
2016-03-11 22:14:15 +0300

    Added files via upload

---
+++ Zadacha1068/Zadacha1068/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1068
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N > 0)
+                for (int i = 0; i < N + 1; i++)
+                    sum = sum + i;
+            else
+                sum = 1;
+                for (int i = 0; i > N - 1; i--)
+                    sum = sum+i;
+            System.Console.WriteLine(sum);
+        }
+    }
+}
