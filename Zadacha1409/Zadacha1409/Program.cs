4d2b560efe1e6d67de750f9058dd7b08eb689318
MKruglov <krumih@mail.ru>
2016-03-11 21:58:26 +0300

    Added files via upload

---
+++ Zadacha1409/Zadacha1409/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Gary = Convert.ToInt32(Console.ReadLine());
+            int Lary = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine((10 - Gary));
+            System.Console.Write((10 - Lary));
+        }
+    }
+}
